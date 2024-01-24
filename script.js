31cfa946ed6779de2f206077d131ca07e547184b
peaga <pauloherbert@alu.ufc.br>
2024-01-24 20:47:21 -0300

    add: encrypt and decrypy functions

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function criptografar(){
+    let text= document.getElementsByClassName('input_text').item(0).value;
+    let output= text.replace(/a/g,"ai");
+    output= output.replace(/e/g,"enter");
+    output= output.replace(/i/g,"imes");
+    output= output.replace(/o/g,"ober");
+    output= output.replace(/u/g,"ufat");
+    document.getElementsByClassName('menu_texto_resultado').item(0).innerHTML=output;
+}
+function descriptografar() {
+    let text= document.getElementsByClassName('input_text').item(0).value;
+    let output= text.replace(/ai/g,"a");
+    output= output.replace(/enter/g,"e");
+    output= output.replace(/imes/g,"i");
+    output= output.replace(/ober/g,"o");
+    output= output.replace(/ufat/g,"u");
+    document.getElementsByClassName('menu_texto_resultado').item(0).innerHTML=output;
+}
